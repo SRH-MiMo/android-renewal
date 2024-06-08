3c07caa9f5ae94feb4b12281fc1156df9e0ea2f0
igiza1213 <igiza1213@gmail.com>
2024-06-08 18:55:52 +0900

    [fix] login 정상 작동

--- app/src/main/java/com/example/mimo/MainActivity.kt
+++ app/src/main/java/com/example/mimo/MainActivity.kt
@@ -44,8 +44,8 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 
 val supabase = createSupabaseClient(
-    supabaseUrl = "", //BuildConfig.SUPABASE_URL,
-    supabaseKey = "", //BuildConfig.SUPABASE_ANON_KEY
+    supabaseUrl = BuildConfig.SUPABASE_URL, //BuildConfig.SUPABASE_URL,
+    supabaseKey = BuildConfig.SUPABASE_ANON_KEY, //BuildConfig.SUPABASE_ANON_KEY
 ) {
     install(Auth)
     install(Postgrest)
